7e98c26354187a5491124fa503887cdeb3f66672
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-03-10 16:47:11 +0300

    columns in config

---
+++ src/data/report-config.js
@@ -0,0 +1,41 @@
+export const config = {
+  name: "Table",
+  code: 0,
+  colums: [
+    {
+      dataField: "id",
+      caption: "id caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    },
+    {
+      dataField: "name",
+      caption: "name caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    },
+    {
+      dataField: "model",
+      caption: "model caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    },
+    {
+      dataField: "manufacturer",
+      caption: "manufacturer caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    },
+    {
+      dataField: "passengers",
+      caption: "passengers caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    },
+  ]
+};
