a4575c19bcf0520c3cefcfa89afe6be7745193d4
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-03-10 16:44:06 +0300

    true config

--- src/report-config.js
+++ src/report-config.js
@@ -1,37 +1,13 @@
 export const config = {
-  $schema: "http://json-schema.org/draft-04/schema#",
-  type: "object",
-  properties: {
-    name: {
-      type: "string",
-    },
-    code: {
-      type: "string",
-    },
-    colums: {
-      type: "array",
-      items: [
-        {
-          type: "object",
-          properties: {
-            dataField: {
-              type: "string",
-            },
-            caption: {
-              type: "string",
-            },
-            dataType: {
-              type: "string",
-            },
-            format: {
-              type: "string",
-            },
-            alignment: {
-              type: "string",
-            },
-          },
-        },
-      ],
-    },
-  },
+  name: "Table",
+  code: 0,
+  colums: [
+    {
+      dataField: "name",
+      caption: "Caption",
+      dataType: "string",
+      format: "UTF-8",
+      alignment: "left",
+    }
+  ]
 };
